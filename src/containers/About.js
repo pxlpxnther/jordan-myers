1004783d919c4483aae7082b65549dfd11806481
Jordan Myers <jordanmyers@Jordans-MBP.hitronhub.home>
2020-06-25 20:43:32 -0600

    Added in image, and started adding content to about page

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import Me from './../assets/img/me.jpg';
 
 const About = () => (
   <div className='about container'>
-    <h1>About</h1>
+    <h1>
+      Hey<span className='accent-color'>.</span>
+    </h1>
+    <div className='img-container'>
+      <img src={Me} />
+    </div>
+    <section>
+      <h2>About Me</h2>
+      <p>
+        So, to start off my name is Jordan Myers, I'm a web developer based in
+        Edmonton Alberta.
+      </p>
+      <h3>Likes</h3>
+      <ul className='flex-container'>
+        <li>🌶</li>
+        <li>🖥</li>
+        <li>🏃‍♂️</li>
+      </ul>
+    </section>
   </div>
 );
 
